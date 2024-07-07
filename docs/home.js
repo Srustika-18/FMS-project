3ff77c873d14c7178a4e0a64bff3fd7290458374
Srustika Mohapatra <142988843+Srustika-18@users.noreply.github.com>
2024-07-07 15:06:11 +0530

    homepage

--- docs/home.js
+++ docs/home.js
@@ -1,3 +1,22 @@
-const reviewContainer = document.querySelector(".reviews-container")
+const reviewContainer = document.querySelector(".reviews-container");
 
-    
+async function getReviews() {
+	const res = await fetch("https://randomuser.me/api/?results=8");
+	const data = await res.json();
+	// console.log(data);
+	for (const review of data.results) {
+		let randomNumber = Math.floor(Math.random() * 5);
+		console.log("🚀 ~ getReviews ~ randomNumber:", randomNumber);
+		console.log(review);
+		reviewContainer.innerHTML += `<div class="review-card">
+						<img src="${review.picture.thumbnail}" alt="User" />
+						<h3>${review.name.first} ${review.name.last}</h3>
+						<div class="stars">${"★".repeat(randomNumber)}${"☆".repeat(
+			5 - randomNumber
+		)}</div>
+						<p>“Add review”</p>
+					</div>`;
+	}
+}
+
+getReviews();
