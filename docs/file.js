ffd73684ab3f0f6dd288968bfe90bdc6fa43b8a9
Srustika <142988843+Srustika-18@users.noreply.github.com>
2024-07-13 09:56:57 +0530

    fetchNotices on file updation

--- docs/file.js
+++ docs/file.js
@@ -1,24 +1,29 @@
 // file.js
 
-import {
-	getCurrentFolderID,
-	getCurrentFolderName,
-	loadFolderContents,
-} from "./folder.js";
+import
+	{
+		getCurrentFolderID,
+		getCurrentFolderName,
+		loadFolderContents,
+	} from "./folder.js";
+import { fetchNotices } from './notices.js'
 import { url } from "./url.js";
 
 export const addFileModal = document.getElementById("addFileModal");
 
-export function showAddFileModal() {
+export function showAddFileModal()
+{
 	addFileModal.showModal();
 }
 
-export function hideAddFileModal() {
+export function hideAddFileModal()
+{
 	addFileModal.close();
 	document.getElementById("newFileName").value = "";
 }
 
-export async function handleAddFile(e) {
+export async function handleAddFile(e)
+{
 	e.preventDefault();
 	const fileName = document.getElementById("newFileName");
 	const fileDescription = document.getElementById("newFileDescription");
@@ -30,58 +35,67 @@ export async function handleAddFile(e) {
 	formData.append("FolderID", getCurrentFolderID());
 	formData.append("description", description);
 
-	try {
+	try
+	{
 		const response = await fetch(
-			`${url}/files/?folder_id=${getCurrentFolderID()}&description=${description}`,
+			`${ url }/files/?folder_id=${ getCurrentFolderID() }&description=${ description }`,
 			{
 				method: "POST",
 				headers: {
-					Authorization: `Bearer ${localStorage.getItem(
+					Authorization: `Bearer ${ localStorage.getItem(
 						"authToken"
-					)}`,
+					) }`,
 				},
 				body: formData,
 			}
 		);
 
-		if (!response.ok) {
+		if (!response.ok)
+		{
 			throw new Error("File upload failed");
 		}
 
 		alert("File uploaded successfully");
 		hideAddFileModal();
 		await loadFolderContents(getCurrentFolderID(), getCurrentFolderName());
-	} catch (error) {
+		await fetchNotices();
+	} catch (error)
+	{
 		alert(
 			"File creation failed: " +
-				error.message +
-				"\nTry logging out then logging in."
+			error.message +
+			"\nTry logging out then logging in."
 		);
 	}
 }
 
-export async function handleDeleteFile(fileId) {
-	try {
-		const response = await fetch(`${url}/files/${fileId}`, {
+export async function handleDeleteFile(fileId)
+{
+	try
+	{
+		const response = await fetch(`${ url }/files/${ fileId }`, {
 			method: "DELETE",
 			headers: {
 				"Content-Type": "application/json",
-				Authorization: `Bearer ${localStorage.getItem("authToken")}`,
+				Authorization: `Bearer ${ localStorage.getItem("authToken") }`,
 			},
 		});
 
-		if (!response.ok) {
+		if (!response.ok)
+		{
 			throw new Error("File deletion failed");
 		}
 
 		alert("File deleted successfully");
 		await loadFolderContents(getCurrentFolderID(), getCurrentFolderName());
-	} catch (error) {
+		await fetchNotices();
+	} catch (error)
+	{
 		console.log("🚀 ~ error:", error);
 		alert(
 			"File deletion failed: " +
-				error.message +
-				"\nTry logging out then logging in."
+			error.message +
+			"\nTry logging out then logging in."
 		);
 	}
 }
